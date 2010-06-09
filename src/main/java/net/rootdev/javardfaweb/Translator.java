ec199261dd35237691b637e269319197f686f1f0
Damian <d.steer@bris.ac.uk>
2010-06-09 17:16:21 +0100

    Updating for 0.4

--- src/main/java/net/rootdev/javardfaweb/Translator.java
+++ src/main/java/net/rootdev/javardfaweb/Translator.java
@@ -8,6 +8,7 @@ package net.rootdev.javardfaweb;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URI;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
@@ -18,12 +19,13 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.StreamingOutput;
-import net.rootdev.javardfa.NTripleSink;
 import net.rootdev.javardfa.ParserFactory;
 import net.rootdev.javardfa.ParserFactory.Format;
-import net.rootdev.javardfa.RDFXMLSink;
 import net.rootdev.javardfa.StatementSink;
 import net.rootdev.javardfa.Version;
+import net.rootdev.javardfa.output.NTripleSink;
+import net.rootdev.javardfa.output.RDFXMLSink;
+import net.rootdev.javardfa.output.TurtleSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.InputSource;
@@ -59,7 +61,7 @@ public class Translator {
             public void write(OutputStream output) throws IOException, WebApplicationException {
                 StatementSink sink = 
                         new RDFXMLSink(output, comment, "Origin: <" + uri + ">");
-                parse(uri, format, sink);
+                parse(uri, format, sink, output);
             }
         };
     }
@@ -83,7 +85,7 @@ public class Translator {
             public void write(OutputStream output) throws IOException, WebApplicationException {
                 StatementSink sink =
                         new NTripleSink(output, comment, "Origin: <" + uri + ">");
-                parse(uri, format, sink);
+                parse(uri, format, sink, output);
             }
         };
     }
@@ -94,7 +96,13 @@ public class Translator {
             @QueryParam("uri") final URI uri,
             @QueryParam("parser")
             @DefaultValue("XHTML") final Format format) {
-        return getNTriples(uri, format);
+        return new StreamingOutput() {
+            public void write(OutputStream output) throws IOException, WebApplicationException {
+                StatementSink sink =
+                        new TurtleSink(output, comment, "Origin: <" + uri + ">");
+                parse(uri, format, sink, output);
+            }
+        };
     }
 
     @POST
@@ -105,7 +113,7 @@ public class Translator {
             public void write(OutputStream output) throws IOException, WebApplicationException {
                 StatementSink sink =
                         new NTripleSink(output, comment, "Origin: POSTed");
-                parse(content, Format.HTML, sink);
+                parse(content, Format.HTML, sink, output);
             }
         };
     }
@@ -118,33 +126,50 @@ public class Translator {
             public void write(OutputStream output) throws IOException, WebApplicationException {
                 StatementSink sink =
                         new NTripleSink(output, comment, "Origin: POSTed");
-                parse(content, Format.XHTML, sink);
+                parse(content, Format.XHTML, sink, output);
             }
         };
     }
 
-    protected void parse(InputStream content, Format format, StatementSink sink) {
+    protected void parse(InputStream content, Format format, StatementSink sink, OutputStream output) {
         InputSource in = new InputSource(content);
         in.setSystemId("urn:invalid:posted");
         in.setEncoding("utf-8");
-        parse(in, format, sink);
+        parse(in, format, sink, output);
     }
 
-    protected void parse(URI url, Format format, StatementSink sink) {
+    protected void parse(URI url, Format format, StatementSink sink, OutputStream output) {
         if (!"http".equals(url.getScheme())) throw new RuntimeException("I only do http");
         InputSource in = new InputSource(url.toString());
         in.setEncoding("utf-8");
-        parse(in, format, sink);
+        parse(in, format, sink, output);
     }
 
-    protected void parse(InputSource in, Format format, StatementSink sink) {
+    protected void parse(InputSource in, Format format, StatementSink sink, OutputStream output) {
         try {
             XMLReader parser = ParserFactory.createReaderForFormat(sink, format);
             parser.parse(in);
         } catch (SAXException ex) {
-            throw new WebApplicationException(ex);
+            midstreamError(ex, output);
         } catch (IOException ex) {
-            throw new WebApplicationException(ex);
+            midstreamError(ex, output);
         }
     }
+
+    private void midstreamError(Exception ex, OutputStream output) {
+        PrintStream p = new PrintStream(output);
+        p.println("###########################################");
+        p.println("######### Mid-stream error ################");
+        ex.printStackTrace(p);
+        p.println("###########################################");
+        try {
+            output.flush();
+        } catch (IOException e) {
+            // What do you expect me to do here??
+            log.error("Flushing went wrong", e);
+        }
+        throw new WebApplicationException(ex);
+    }
+
+
 }
